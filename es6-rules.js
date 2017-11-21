6e80505ab6cb8392f287f1e41462beb6ef25cd47
yeongtaik byeon <yt.byeon@samsung.com>
2017-11-21 11:27:08 +0900

    Update es6-rules.js

--- es6-rules.js
+++ es6-rules.js
@@ -47,6 +47,15 @@ function foo() {
     class A {};
 }
 
+function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
+    var x = -1;
+    if (x = -1) console.log('Error!', x);
+}
+
+function CWE_482_665() { // UNUSED_EXPR
+    this.foo;
+}
+
 import {
     count,
     increment // UNUSED_IMPORT alarm
