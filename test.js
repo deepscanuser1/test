eaa9206f4e7710a0fa99ba32315c89323a507090
deepscan <demodeepscan@gmail.com>
2017-11-23 17:24:48 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,30 @@
-function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
-    var x = -1;
-    if (x = -1) console.log('Error!', x);
-}
+
 
 function CWE_482_665() { // UNUSED_EXPR
     this.foo;
 }
 
-var a = 0;
-var b = 0;
+// Example 1
+function foo() {
+    var obj == {};
+    var y = obj.x; // NULL_POINTER alarm
+}
+ 
+// Example 2
+if (x == null) { // (x == null) should be modified to (x != null).
+    y = x.a; // NULL_POINTER alarm: x is undefined or null but is property-accessed.
+}
+ 
+// Example 3
+y = x || x.a; // NULL_POINTER alarm: x has a falsy value but is property-accessed.
+ 
+// Example 1
+function foo() {
+    var obj;
+    var y = obj.x; // NULL_POINTER alarm
+}
+ 
+// Example 2
+if (x == null) { // (x == null) should be modified to (x != null).
+    y = x.a; // NULL_POINTER alarm: x is undefined or null but is property-accessed.
+}
